ebd5ea935254146c04e2c66565945609df515478
Epyi <epyi.contact@gmail.com>
2023-08-20 15:12:35 +0200

    fix(Overall): Add portable jquery, remove unnecessary usage of jquery

--- html/js/script.js
+++ html/js/script.js
@@ -1,18 +1,16 @@
 window.addEventListener('message', function(event) {
     let data = event.data
     if (event.data.type == 'showui') {
-        $(".coords-menu").fadeIn(200);
+        document.getElementById("coords-menu-one").style.display = "block"
     }
     if (event.data.type == 'hideui') {
-        $(".coords-menu").fadeOut();
+        document.getElementById("coords-menu-one").style.display = "none"
     }
     if (event.data.type == 'showNotification') {
         const notificationContainer = document.getElementById('notificationContainer');
         notificationContainer.textContent = event.data.message;
-        $(".notification").fadeIn(200);
-        setTimeout(function () {
-            $(".notification").fadeOut();
-        }, 2000);
+        $("#notificationContainer").fadeIn()
+        $("#notificationContainer").fadeOut()
     }
     if (event.data.type == 'copy') {
         const el = document.createElement('textarea');
@@ -22,60 +20,61 @@ window.addEventListener('message', function(event) {
         document.execCommand('copy');
         document.body.removeChild(el);
     }
+})
 
-    $("#close").click(function() {
-        $.post("https://epyi_hud/exit", JSON.stringify({}));
-    })
+$(document).ready(function () {
+    document.getElementById("close").addEventListener("click", () => {
+        $.post("https://epyi_coords/exit", JSON.stringify({}));  
+    });
 
-
-    $("#c-vec3").click(function() {
-        $.post("https://epyi_hud/request-copy", JSON.stringify({
+    document.getElementById("c-vec3").addEventListener("click", () => {
+        $.post("https://epyi_coords/request-copy", JSON.stringify({
             type: "c-vec3"
         }));
-    })
-    $("#c-table").click(function() {
-        $.post("https://epyi_hud/request-copy", JSON.stringify({
+    });
+    document.getElementById("c-table").addEventListener("click", () => {
+        $.post("https://epyi_coords/request-copy", JSON.stringify({
             type: "c-table"
         }));
-    })
-    $("#c-tablei").click(function() {
-        $.post("https://epyi_hud/request-copy", JSON.stringify({
+    });
+    document.getElementById("c-tablei").addEventListener("click", () => {
+        $.post("https://epyi_coords/request-copy", JSON.stringify({
             type: "c-tablei"
         }));
-    })
-    $("#c-X").click(function() {
-        $.post("https://epyi_hud/request-copy", JSON.stringify({
+    });
+    document.getElementById("c-X").addEventListener("click", () => {
+        $.post("https://epyi_coords/request-copy", JSON.stringify({
             type: "c-X"
         }));
-    })
-    $("#c-Y").click(function() {
-        $.post("https://epyi_hud/request-copy", JSON.stringify({
+    });
+    document.getElementById("c-Y").addEventListener("click", () => {
+        $.post("https://epyi_coords/request-copy", JSON.stringify({
             type: "c-Y"
         }));
-    })
-    $("#c-Z").click(function() {
-        $.post("https://epyi_hud/request-copy", JSON.stringify({
+    });
+    document.getElementById("c-Z").addEventListener("click", () => {
+        $.post("https://epyi_coords/request-copy", JSON.stringify({
             type: "c-Z"
         }));
-    })
-    $("#h-W").click(function() {
-        $.post("https://epyi_hud/request-copy", JSON.stringify({
+    });
+    document.getElementById("h-W").addEventListener("click", () => {
+        $.post("https://epyi_coords/request-copy", JSON.stringify({
             type: "h-W"
         }));
-    })
-    $("#ch-vec4").click(function() {
-        $.post("https://epyi_hud/request-copy", JSON.stringify({
+    });
+    document.getElementById("ch-vec4").addEventListener("click", () => {
+        $.post("https://epyi_coords/request-copy", JSON.stringify({
             type: "ch-vec4"
         }));
-    })
-    $("#ch-table").click(function() {
-        $.post("https://epyi_hud/request-copy", JSON.stringify({
+    });
+    document.getElementById("ch-table").addEventListener("click", () => {
+        $.post("https://epyi_coords/request-copy", JSON.stringify({
             type: "ch-table"
         }));
-    })
-    $("#ch-tablei").click(function() {
-        $.post("https://epyi_hud/request-copy", JSON.stringify({
+    });
+    document.getElementById("ch-tablei").addEventListener("click", () => {
+        $.post("https://epyi_coords/request-copy", JSON.stringify({
             type: "ch-tablei"
         }));
-    })
+    });
 })
